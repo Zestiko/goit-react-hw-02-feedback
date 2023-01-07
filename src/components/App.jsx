996dc1e727a4ca43ab4e422bef3ccfae0b63106c
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-01-07 15:38:37 +0200

    Close to finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import React, { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+export class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+
+  handleClick = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));    
+  }
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    let tottalFeedback = good + neutral + bad;
+    return Math.round(100 * good / tottalFeedback);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <div className="button-list">
+          <button name='good' type='button' onClick={this.handleClick}>good</button>
+          <button name='neutral' type='button' onClick={this.handleClick}>Neutral</button>
+          <button name='bad' type='button'onClick={this.handleClick}>bad</button>
+        </div>
+        <Statistics />
+      </div>  
   );
+  }
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,10 @@
+export const Statistics = () => {
+    return <div>
+      <h2>Statistic</h2>
+      <p>good: </p>
+      <p>Neutral: </p>
+      <p>bad: </p>
+      <p>Total:</p>
+      <p>Positive feedback:</p>
+    </div>
+}
